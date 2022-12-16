9d094c4a63a43c3d4f7d8db97deeae597a8b649a
Raphael <rxphxel7k@gmail.com>
2022-12-16 20:07:36 -0300

    responsividade

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -3,26 +3,9 @@
     <v-row class="size-all d-flex justify-md-center">
       <v-col
         cols="12"
-        md="3"
-        class="flex ml-n16 h-full d-flex align-center mt-n16"
-      >
-        <div
-          class="
-            shadow-fx-1
-            mt-n10 mt-md-2
-            avatar
-            mx-auto
-            ml-md-0 ml-n16
-            mb-md-0 mb-n16
-          "
-          :style="$vuetify.breakpoint.mobile ? 'transform: scale(0.9)' : ''"
-        ></div>
-      </v-col>
-      <v-col
-        cols="12"
         md="7"
         class="
-          mt-n16 mt-md-0
+          mt-8 mt-md-0
           ml-md-n4
           pl-md-16
           flex
@@ -40,22 +23,6 @@
             >Raphael Ferreira Bertoldo, 24 anos, Uberlândia-MG</v-card-title
           >
           <v-card-text>
-            <!-- Desde muito novo fui interessado em tecnólogia, tive contato com
-            comptador e internet muito cedo, desde então comecei os meus
-            primeiros projetos apenas usando HTML e CSS na época dos blogs, logo
-            ápos eu e meu irmão tentamos recriar um jogo online que a gente
-            jogava, assim eu tive a noção da estrutura e arquitetura de um
-            projeto, meu irmão continuou na programação, e eu segui o caminho da
-            música, aprendi tocar vários instrumentos, aprendi produzir próprias
-            músicas, realizei o sonho de várias pessoas em ter sua música
-            registrada e profissional, hoje a música e apenas um hobbie, faço
-            porque amo, pois fui atraído pela área de tecnólogia e me dei super
-            bem, consegui aprender com muita facilidade por já ter uma boa noção
-            de computadores, lógica, e arquitetura de software, eu tive a
-            oportunidade de trabalhar em vários projetos, com várias pessoas
-            diferentes e com várias tecnólogias diferentes támbem, estou sempre
-            em busca de novos desafios para agregar na minha carreira e para
-            provar pra eu mesmo o quanto sou capaz ! -->
             Trabalho com desenvlvimento de software há 3 anos, comecei como
             desenvolvedor front-end, hoje sou Full-Stack em uma empresa de
             marketing, onde desenvolvemos e mantemos um e-commerce, um produto
@@ -103,6 +70,24 @@
             </v-col>
           </v-row>
         </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        md="3"
+        class="mb-0 mb-md-14 flex ml-n16 h-full d-flex align-center mt-n16"
+      >
+        <v-img src="../assets/ReadyPlayerMe-Avata-2.png"></v-img>
+        <!-- <div
+          class="
+            shadow-fx-1
+            mt-n10 mt-md-2
+            avatar
+            mx-auto
+            ml-md-0 ml-n16
+            mb-md-0 mb-n16
+          "
+          :style="$vuetify.breakpoint.mobile ? 'transform: scale(0.9)' : ''"
+        ></div> -->
       </v-col>
     </v-row>
   </div>
