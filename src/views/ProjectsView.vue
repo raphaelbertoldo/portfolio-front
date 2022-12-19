a1f0c2db36d536001cce9f8d76f964d9bb3b2202
Raphael <rxphxel7k@gmail.com>
2022-12-19 14:59:02 -0300

    responsividade

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -9,20 +9,97 @@
       </h1>
     </div>
     <v-row class="px-4 px-md-10">
-      <v-col cols="12" md="3" v-for="(proj, i) in projects" :key="i">
+      <!-- cols="12" -->
+      <v-col
+        cols="12"
+        :md="
+          $vuetify.breakpoint.width < 1800
+            ? '6'
+            : '' && $vuetify.breakpoint.width < 900
+            ? '12'
+            : '3'
+        "
+        v-for="(proj, i) in projects"
+        :key="i"
+        class=""
+      >
+        <v-card
+          :height="$vuetify.breakpoint.mobile ? '' : '320px'"
+          class="
+            pa-2 pa-md-8
+            d-flex
+            align-content-space-between
+            flex-wrap
+            justify-center
+            glass-fx
+          "
+          dark
+        >
+          <v-card-text class="mx-auto">
+            <!-- <v-card-title class="text-center"> {{ proj.name }}</v-card-title> -->
+            <h2 class="mb-6 text-center">{{ proj.name }}</h2>
+            <div class="d-flex">
+              <v-img
+                :aspect-ratio="1 / 1"
+                class="mr-4 shadow-2 rounded-xl mx-auto"
+                :src="proj.img"
+                :style="
+                  $vuetify.breakpoint.mobile
+                    ? 'max-width: 100px; max-height: 100px; min-width: 100px; min-height: 100px'
+                    : 'max-width: 120px; max-height: 120px; min-width: 120px; min-height: 120px'
+                "
+              />
+              {{ proj.description }}
+            </div>
+          </v-card-text>
+          <v-card-actions class="d-flex flex">
+            <div class="mx-auto flex d-flex">
+              <a
+                class="mx-2"
+                v-if="proj.link"
+                :href="proj.link"
+                target="blank"
+                style="text-decoration: none"
+              >
+                <v-btn
+                  block
+                  outlined
+                  color="secondary"
+                  class="black--text block d-flex flex"
+                  >Acessar
+                </v-btn>
+              </a>
+              <a
+                class="mx-2"
+                v-if="proj.repository"
+                :href="proj.repository"
+                target="blank"
+                style="text-decoration: none"
+              >
+                <v-btn
+                  block
+                  outlined
+                  color="primary"
+                  class="black--text block d-flex flex"
+                  >Repositório
+                </v-btn>
+              </a>
+            </div>
+          </v-card-actions>
+        </v-card>
         <!-- <v-card dark class="glass-fx ma-4 mx-10 mt-10 pa-6 glass=fx"
           >proj.name</v-card
         > -->
-        <v-card
+        <!-- <v-card
           :max-height="$vuetify.breakpoint.mobile ? '' : '250px'"
           min-height="450px"
           dark
           class="align-center justify-center d-flex glass-fx rounded-xl"
         >
           <div
-            class="mx-4 d-flex align-center justify-center"
+            class="red flex mx-4 d-flex align-center justify-center"
             style="height: auto"
-            :class="$vuetify.breakpoint.mobile ? 'd-flex flex-column' : ''"
+            :class="$vuetify.b  reakpoint.mobile ? 'd-flex flex-column' : ''"
           >
             <div class="ma-md-4 my-md-8 my-0 mt-6 ma-0"></div>
             <div key="" class="my-4">
@@ -80,7 +157,7 @@
               </v-card-subtitle>
             </div>
           </div>
-        </v-card>
+        </v-card> -->
       </v-col>
     </v-row>
   </div>
